9bc61affc791541b8446cb09c86bb32750d4ddb6
GOWEY <alaisney7@gmail.com>
2024-04-19 11:01:52 +0200

    Test 1

---
+++ dist/script.js
@@ -0,0 +1,2 @@
+function maFonction(){alert("Bonjour, je suis une fonction de ma bibliothèque.");}var maVariable="Ceci est une variable de ma bibliothèque.";
+//# sourceMappingURL=mon_script.min.js.map
---
+++ src/script.js
@@ -0,0 +1,10 @@
+// Définition d'une fonction
+function maFonction() {
+    alert("Bonjour, je suis une fonction de ma bibliothèque.");
+}
+
+// Définition d'une variable
+var maVariable = "Ceci est une variable de ma bibliothèque.";
+
+// Export des fonctions ou variables que vous souhaitez rendre publiques
+export { maFonction, maVariable };
